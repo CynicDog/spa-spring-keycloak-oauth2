ed6644bdecdac519eb2ed9227c96cb0e20e5ca59
이은상 <kyrsang0801@inbrein.com>
2024-09-27 09:46:28 +0900

    Chore: removed GitHub Pages hosting configuration

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/spa-spring-keycloak-oauth2", plugins: [react()], })
+export default defineConfig({
+  plugins: [react()],
+})
