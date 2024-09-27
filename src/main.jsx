39bc5cd9b24f07cc638fc17840feeacaccd534e5
ginsenglee <thecynicdog0328@gmail.com>
2024-09-28 08:53:10 +0900

    Fix: added missing QueryClientProvider setup for React Query

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {AuthProvider} from "../Context.jsx";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>
-)
+    <QueryClientProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </QueryClientProvider>
+);
